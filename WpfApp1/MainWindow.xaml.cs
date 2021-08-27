1657c22cab34cbc4e8810692479fd4ae2ff26ecd
gui-marchioro <89101832+gui-marchioro@users.noreply.github.com>
2021-08-27 10:35:49 -0300

    Added new Task Class and Data Binding

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace WpfApp1
 {
@@ -20,16 +21,25 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Tasks tListNotStarted = new Tasks();
+        Tasks tListInProgress = new Tasks();
+        Tasks tListCompleted = new Tasks();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
+
+        public ObservableCollection<Task> TListNS { get { return tListNotStarted.TaskList; } }
+        public ObservableCollection<Task> TListIP { get { return tListInProgress.TaskList; } }
+        public ObservableCollection<Task> TListC { get { return tListCompleted.TaskList; } }
 
         private void btAddTaskHandler(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(tbTask.Text) && !lbNotStarted.Items.Contains(tbTask.Text))
             {
-                lbNotStarted.Items.Add(tbTask.Text);
+                tListNotStarted.TaskList.Add(new Task(tbTask.Text));
                 tbTask.Clear();
             }
             UnselectAll();
@@ -60,8 +70,47 @@ namespace WpfApp1
         }
         void ChangeListBox(ListBox sender, ListBox receiver)
         {
-            receiver.Items.Add(sender.SelectedItem);
-            sender.Items.Remove(sender.SelectedItem);
+            int index = sender.SelectedIndex;
+            //System.Windows.MessageBox.Show("{0}", index.ToString()); //DEBUG
+            if (receiver.Name == "lbNotStarted")
+            {
+                if (sender.Name == "lbInProgress")
+                {
+                    tListNotStarted.TaskList.Add(tListInProgress.TaskList.ElementAt<Task>(index));
+                    tListInProgress.TaskList.Remove(tListInProgress.TaskList.ElementAt<Task>(index));
+                }
+                else if (sender.Name == "lbCompleted")
+                {
+                    tListNotStarted.TaskList.Add(tListCompleted.TaskList.ElementAt<Task>(index));
+                    tListCompleted.TaskList.Remove(tListCompleted.TaskList.ElementAt<Task>(index));
+                }
+            }
+            else if (receiver.Name == "lbInProgress")
+            {               
+                if (sender.Name == "lbNotStarted")
+                {
+                    tListInProgress.TaskList.Add(tListNotStarted.TaskList.ElementAt<Task>(index));
+                    tListNotStarted.TaskList.Remove(tListNotStarted.TaskList.ElementAt<Task>(index));
+                }
+                else if (sender.Name == "lbCompleted")
+                {
+                    tListInProgress.TaskList.Add(tListCompleted.TaskList.ElementAt<Task>(index));
+                    tListCompleted.TaskList.Remove(tListCompleted.TaskList.ElementAt<Task>(index));
+                }
+            }
+            else if (receiver.Name == "lbCompleted")
+            {
+                if (sender.Name == "lbNotStarted")
+                {
+                    tListCompleted.TaskList.Add(tListNotStarted.TaskList.ElementAt<Task>(index));
+                    tListNotStarted.TaskList.Remove(tListNotStarted.TaskList.ElementAt<Task>(index));
+                }
+                else if (sender.Name == "lbInProgress")
+                {
+                    tListCompleted.TaskList.Add(tListInProgress.TaskList.ElementAt<Task>(index));
+                    tListInProgress.TaskList.Remove(tListInProgress.TaskList.ElementAt<Task>(index));
+                }
+            }
             UnselectAll();
         }
 
---
+++ WpfApp1/Task.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public int Priority { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+
+        public override string ToString()
+        {
+            // return base.ToString();
+            return string.Format("{0}", Name);
+        }
+
+        public Task(string name)
+        {
+            this.Name = name;
+            this.Priority = 1;
+            this.State = "Not Started";
+        }
+        public Task(string name, int priority)
+        {
+            this.Name = name;
+            this.Priority = priority;
+            this.State = "Not Started";
+        }
+        public Task(string name, int priority, string description)
+        {
+            this.Name = name;
+            this.Priority = priority;
+            this.Description = description;
+            this.State = "Not Started";
+        }
+    }
+}
